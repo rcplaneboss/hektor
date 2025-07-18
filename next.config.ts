dc64bd23f40a7be5a3c86fce82c072b7556484b1
Abdul Rahman Qosim <157493741+rcplaneboss@users.noreply.github.com>
2025-07-18 17:28:23 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("https://lh3.googleusercontent.com/**")],
   },
+  const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
