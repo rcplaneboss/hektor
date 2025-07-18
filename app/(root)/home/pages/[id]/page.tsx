41efbaaff54176514764ad486591cbfb759c3327
Abdul Rahman Qosim <157493741+rcplaneboss@users.noreply.github.com>
2025-07-18 18:08:23 +0100

    Update page.tsx

--- app/(root)/home/pages/[id]/page.tsx
+++ app/(root)/home/pages/[id]/page.tsx
@@ -1,20 +1,22 @@
-"use client"
-import React from 'react'
-import pathAppend from "@/app/utils/utils"
-import {useEffect} from "react";
-import { usePathname } from "next/navigation"
+"use client";
+import React, { useEffect } from "react";
+import pathAppend from "@/app/utils/utils";
+import { usePathname } from "next/navigation";
 
-
-
-const Page = ( params  : Promise<{ params: { id: string}}>) => {
-    const pathname = usePathname();
-    useEffect(()=>{
-        pathAppend("my-account");
-    },[pathname])
-
-    return (
-        <main>User: {params.id}</main>
-
-    )
+interface PageProps {
+  params: {
+    id: string;
+  };
 }
-export default Page
+
+const Page = ({ params }: PageProps) => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    pathAppend("my-account");
+  }, [pathname]);
+
+  return <main>User: {params.id}</main>;
+};
+
+export default Page;
