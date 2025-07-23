a4ff3f3518fad12feaa791c815a2240f3d507008
rcplaneboss <qosimabdulrahman0@gmail.com>
2025-07-23 13:28:38 -0700

    fix dynamic route [id]

--- app/(root)/home/pages/[id]/page.tsx
+++ app/(root)/home/pages/[id]/page.tsx
@@ -1,17 +1,17 @@
-// "use client";
-// import React, { useEffect } from "react";
-// import { usePathname } from "next/navigation";
-// import pathAppend from "@/app/utils/utils";
+"use client";
+import React, { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import pathAppend from "@/app/utils/utils";
 
-// const Page = () => {
-//   const pathname = usePathname();
-//   const id = pathname?.split("/").pop();
+const Page = () => {
+  // const pathname = usePathname();
+  // const id = pathname?.split("/").pop();
 
-//   useEffect(() => {
-//     pathAppend("my-account");
-//   }, [pathname]);
+  // useEffect(() => {
+  //   pathAppend("my-account");
+  // }, [pathname]);
 
-//   return <main>User: {id}</main>;
-// };
+  return <main>User: </main>;
+};
 
-// export default Page;
+export default Page;
