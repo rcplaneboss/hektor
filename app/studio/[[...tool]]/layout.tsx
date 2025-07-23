178e9d25352686e56437c9037356675d1ec0cecf
rcplaneboss <qosimabdulrahman0@gmail.com>
2025-07-23 13:18:44 -0700

    fix /studio

---
+++ app/studio/[[...tool]]/layout.tsx
@@ -0,0 +1,8 @@
+// app/studio/layout.tsx
+export default function StudioLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>; // or wrap in actual layout structure
+}
